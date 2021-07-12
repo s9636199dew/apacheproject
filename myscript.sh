7ebd83c59aa50cb5b4c02dd1062e3af3cde5bcb5
Saikrishnaprasad racha <saikrishnaprasadracha@Sracha.local>
2021-07-12 13:00:09 -0400

    llll

--- myscript.sh
+++ myscript.sh
@@ -1,1 +1,12 @@
 #!bin/bash/
+apt update -y
+apt install apache2 -y
+apt install git -y
+
+#cloning the code
+git clone https://github.com/s9636199dew/apacheproject.git
+
+#copying the index.html
+cp -v apacheproject/index.html /var/www/html
+# restart apache
+systemctl restart apache
